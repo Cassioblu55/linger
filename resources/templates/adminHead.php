3db01f2f3c31be4535631e68ad5be00f6b061fe0
Cassio Hudson <cassioblubyrd@gmail.com>
2016-01-15 00:14:20 -0600

    s

--- resources/templates/adminHead.php
+++ resources/templates/adminHead.php
@@ -1,12 +1,13 @@
-<?php $defaultTitle="Linger Admin"?>
+<?php include_once $serverPath.'admin/login/requireAdmin.php';
+$defaultTitle="Linger Admin"?>
 
 <!doctype html>
+
 <html ng-app="app">
 <script type="text/javascript">var baseURL = "<?php echo $baseURL;?>";</script>
 <title><?php if(isset($title)){echo $title;}else{echo $defaultTitle;}?></title>
 
 <?php 
-include_once $serverPath.'admin/login/requireAdmin.php';
 
 include_once $serverPath.'resources/templates/header.php';?>
 <link rel="stylesheet" href="<?php echo $baseURL;?>resources/adminLayout.css"/>	
